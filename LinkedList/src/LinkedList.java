2ebc237fc82f6b17a0ca536a90e0cc2cfb3e93db
liukeng <1716536548@qq.com>
2021-03-29 12:41:30 +0800

    删除测试的东西

--- LinkedList/src/LinkedList.java
+++
@@ -1,6 +0,0 @@
-public class LinkedList {
-    LNode head;//头结点
-    LNode cur;//当前指向
-
-
-}
--- LinkedList/src/test.java
+++
@@ -1,9 +0,0 @@
-
-
-public class test {
-    public static void main(String[] args) {
-        int num;
-        num=Method.InputNum();
-        System.out.println(num);
-    }
-}
